79b8e26dc55562f3796e1c0758219c2568393fab
叶宝山 <504165808@qq.com>
2018-12-07 22:33:07 +0800

    功能区分

--- main_runner.py
+++ main_runner.py
@@ -25,24 +25,24 @@ class DejavuRunner(object):
 	def __init__(self):
 		with open("dejavu.cnf.SAMPLE") as f:
 			self.config = json.load(f)
+			# create a Dejavu instance
 			self.djv = Dejavu(self.config)
-			
+
+	def fingerprints_saver(self, msc_path=r"Q:\huawei\huawei-dejavu\mp3", msc_type=".mp3"):
+		# 读取磁盘进行存储指纹
+		print('正在存储指纹.....')
+		self.djv.fingerprint_directory(msc_path, [msc_type], 3)
+		print('音乐波谱指纹存储完毕！')
+
 	def file_recognizer_func(self, what_file):
 		"""
-
 		:param what_file: 需要被识别的音乐文件路径
 		:return:
 		"""
-		# create a Dejavu instance
-		
-		# mp3_to_wav
+
 		# 更改源码后  支持直接添加MP3指纹
 		# Fingerprint all the wav's in the directory we give it
 
-		# 读取磁盘进行存储指纹 并识别
-		print('正在存储指纹.....')
-		self.djv.fingerprint_directory(r"Q:\huawei\huawei-dejavu\mp3", [".mp3"], 3)
-		print('音乐波谱指纹存储完毕！')
 		print(u'正在识别指定音乐·······')
 		song = self.djv.recognize(FileRecognizer, '', '', what_file,)
 		print(u'已经识别出指定音乐！')
@@ -69,7 +69,6 @@ class DejavuRunner(object):
 		# 基础录音路径
 		record_path = 'pub_utils/sound_recording/sound_rds'
 		song = self.djv.recognize(MicrophoneRecognizer, record_path=record_path, record_id=record_id, seconds=secs)
-
 
 		if song is None:
 			print("Nothing recognized -- did you play the song out loud so your mic could hear it? :)")
@@ -98,7 +97,7 @@ class DejavuRunner(object):
 # 模拟调用录音
 if __name__ == '__main__':
 	what_file = r'Q:\huawei\huawei-dejavu\mp3\Sean-Fournier--Falling-For-You.mp3'
-	# DejavuRunner().file_recognizer_func(what_file=what_file)
-	# DejavuRunner().sound_record_recognizer_func()
+	DejavuRunner().file_recognizer_func(what_file=what_file)
+	DejavuRunner().sound_record_recognizer_func()
 	is_here = DejavuRunner().record_searcher(music_name='Sean-Fournier--Falling-For-You')
 	print(is_here)
---
+++ test3.py
@@ -0,0 +1,5 @@
+from main_runner import DejavuRunner
+
+if __name__ == '__main__':
+
+    DejavuRunner().file_recognizer_func('new_mp3/Sean-Fournier--Falling-For-You-short2.mp3')
